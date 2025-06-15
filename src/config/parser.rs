770a56154fc128409afb220c5eeabc21b3be3559
tachu <roatyungtatyu@gmail.com>
2025-06-15 13:29:16 +0900

    add: parser

--- src/config/parser.rs
+++ src/config/parser.rs
@@ -2,6 +2,8 @@ use std::collections::LinkedList;
 
 use crate::errors::ConfigParseError;
 
+use super::program::program::AutoRestart;
+
 pub struct ProgramParser;
 
 impl ProgramParser {
@@ -21,5 +23,22 @@ impl ProgramParser {
         numprocs
             .parse::<u8>()
             .map_err(|_| ConfigParseError::UnexpectedValue(numprocs.to_string()))
+    }
+
+    pub fn parse_autostart(auto_start: &str) -> Result<bool, ConfigParseError> {
+        match auto_start.to_lowercase().as_str() {
+            "true" => Ok(true),
+            "false" => Ok(false),
+            _ => Err(ConfigParseError::UnexpectedValue(auto_start.to_string())),
+        }
+    }
+
+    pub fn parse_autorestart(autorestart: &str) -> Result<AutoRestart, ConfigParseError> {
+        match autorestart.to_lowercase().as_str() {
+            "true" => Ok(AutoRestart::True),
+            "false" => Ok(AutoRestart::False),
+            "unexpected" => Ok(AutoRestart::Unexpected),
+            _ => Err(ConfigParseError::UnexpectedValue(autorestart.to_string())),
+        }
     }
 }
